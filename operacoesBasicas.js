a3e11d93c83a06250c36a3b666a378f3fe07a1d1
Nataly Rodrigues <134534295+natalyrodrigues@users.noreply.github.com>
2024-10-24 16:19:27 -0300

    template strings

---
+++ operacoesBasicas.js
@@ -0,0 +1,9 @@
+function calcularOperacoes(num1, num2) {
+
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
+console.log(`${num1} - ${num2} = ${num1 - num2}`);
+console.log(`${num1} x ${num2} = ${num1 * num2}`);
+console.log(`${num1} / ${num2} = ${num1 / num2}`);
+}
+// Exemplo de uso da função
+calcularOperacoes(4, 5);
